cbb92174a24505e41544aec78847968f6d882fa9
koki-maekawa <koki.study33@gmail.com>
2025-07-16 11:09:49 +0900

    add supabase setting

--- next.config.ts
+++ next.config.ts
@@ -8,6 +8,11 @@ const nextConfig: NextConfig = {
         protocol: 'https',
         hostname: 'picsum.photos',
       },
+      {
+        protocol: 'https',
+        hostname: 'iptphizotklvhwfqkodc.supabase.co'
+        
+      }
     ],
   },
   experimental: {
